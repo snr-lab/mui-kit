38f81a93371c4b21b24c7a64df948d3836077621
Rahul Kundu <snrlab.in@gmail.com>
2022-11-29 16:51:27 +0530

    Adding icon to switch

--- src/components/AsyncSwitch.tsx
+++ src/components/AsyncSwitch.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Switch, SwitchProps, CircularProgress } from '@material-ui/core';
 
@@ -11,24 +12,45 @@ const useStyles = makeStyles((theme) => ({
         position: "absolute",
         top: 10,
         left: 20
+    },
+    hiddenSwitch: {
+        opacity: 0
+    },
+    iconSwitch: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
     }
 }));
 
 interface AsyncSwitchProps extends SwitchProps{
     disabled?: boolean;
     updating?: boolean;
+    onIcon?: React.ReactElement<any, any>,
+    offIcon?: React.ReactElement<any, any>
 }
 
 export const AsyncSwitch: React.FC<AsyncSwitchProps> = (props) => {
-    const {disabled, updating, ...switchProps} = props
+    const {disabled, updating, onIcon, offIcon, ...switchProps} = props
     const classes = useStyles();
     
     return (
         <Box className={classes.switchContainer}>
-            <Switch
-                {...switchProps}
-                disabled={disabled || updating}
-            />
+            {onIcon && offIcon && <div className={classes.iconSwitch}>
+                {switchProps.checked && onIcon}
+                {!switchProps.checked && offIcon}
+            </div>}
+            <div className={clsx(onIcon && offIcon && classes.hiddenSwitch)}>
+                <Switch
+                    {...switchProps}
+                    disabled={disabled || updating}
+                />
+            </div>
             {updating && <CircularProgress size={20} className={classes.updating} />}
         </Box>
     );
